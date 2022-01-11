2b4e258106dfa6be34e874e6cfcb25f58cabed3e
Frank Wang <ffwang2@gmail.com>
2022-01-11 00:35:38 +0000

    up to sources module in dbt fundamentals

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,5 +1,11 @@
 select
-    order_id,
-    amount
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
 
-from raw.stripe.payment
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from {{ source('stripe','payment')}}
